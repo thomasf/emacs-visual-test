0aa3505e37d90bcd6a470da7d9eef14b66ca03c8
Thomas Frössman <thomasf@jossystem.se>
2013-04-16 12:07:16 +0200

    simple action sequence

--- emacs-visual-test.el
+++ emacs-visual-test.el
@@ -5,13 +5,34 @@
 
 (load-file "test-emacsd/init.el")
 
-
 (defun visual-test-find-file (name)
-  "DOCSTRING"
-  (find-file (expand-file-name name "test-files")
-))
+  (find-file (expand-file-name name "test-files")))
 
+(defun visual-test-screenshot ()
+  (call-process "scrot" nil nil nil "-u" "screenshot-%Y-%m-%d_%H-%M-%S_$wx$h.png")
+  (message "saved screenshot"))
+
+;; set theme
+(load-theme 'solarized-dark t)
+;; open a file
 (visual-test-find-file "django-template.html")
+;;  go to some position
+(goto-char 27)
+;; Prepare taking a screenshot
+(run-with-idle-timer 2 nil
+                     '(lambda ()
+                        (visual-test-screenshot)
+                        (keyboard-quit)
+                        (kill-buffer)
+                        ))
+;; open ispell-complete word interaction
+(call-interactively 'ispell-complete-word)
+
+;;
+;; TODO async queueing of actions
+;;
+
+
 
 
 (provide 'emacs-visual-test)
--- test-emacsd/init.el
+++ test-emacsd/init.el
@@ -36,5 +36,5 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
-(load-theme 'solarized-dark t)
+
 
